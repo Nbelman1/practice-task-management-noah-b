82b490284194f01ad88ec54d7258ce7475f00348
Noah <nbelman1@gmail.com>
2025-11-01 19:54:51 -0400

    Added useParams hook, find function, element rendering

--- task-management/src/components/TaskDetail.jsx
+++ task-management/src/components/TaskDetail.jsx
@@ -1,9 +1,23 @@
 // displays details of a specific task
-const TaskDetail = () => {
+
+import { useParams } from "react-router-dom";
+
+const TaskDetail = ({ tasks }) => {
+
+    // grab ID from URL
+    const { id } = useParams();
+
+    // return task that matches ID
+    const fetchTask = tasks.find(task => task.id == id);
+
+    // TODO: logic to handle no matching ID error gracefully
+
+    // render task details
     return (
-        <>
-            
-        </>
+        <div>
+            <h3>Task: {fetchTask.title}</h3>
+            <p><strong>Description</strong>: {fetchTask.description}</p>
+        </div>
     );
 };
 
