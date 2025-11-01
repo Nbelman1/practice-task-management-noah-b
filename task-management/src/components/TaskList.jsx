07200219c95ce5ddb2cd221584b960a6a83637aa
Noah <nbelman1@gmail.com>
2025-11-01 19:55:38 -0400

    Added map function, element rendering

--- task-management/src/components/TaskList.jsx
+++ task-management/src/components/TaskList.jsx
@@ -1,9 +1,23 @@
 // displays a list of tasks
-const TaskList = () => {
+
+import { Link } from 'react-router-dom';
+
+const TaskList = ({ tasks }) => {
+    
+    const renderTasks = tasks.map((task) => 
+        <li key={task.id}>
+            <Link to={`/tasks/${task.id}`}>{task.title}</Link>
+        </li>
+    );
+    
+    // render task links
     return (
-        <>
-            
-        </>
+        <div>
+            <h2>Tasks</h2>
+            <ul>
+                {renderTasks}
+            </ul>
+        </div>
     );
 };
 
