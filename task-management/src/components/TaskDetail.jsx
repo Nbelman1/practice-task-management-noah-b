6a67836838ddeb52f0435197fb1837f173e1ee0a
Noah <nbelman1@gmail.com>
2025-11-01 20:29:37 -0400

    Added error handling logic

--- task-management/src/components/TaskDetail.jsx
+++ task-management/src/components/TaskDetail.jsx
@@ -10,13 +10,25 @@ const TaskDetail = ({ tasks }) => {
     // return task that matches ID
     const fetchTask = tasks.find(task => task.id == id);
 
-    // TODO: logic to handle no matching ID error gracefully
+    const handleIds = (id) => {
+        if (id > tasks.length || id == 0) {
+            return "Invalid task, please try again.";
+        } else if (!id) {
+            return "Unknown task, please try again.";
+        } else if (id) {
+            return (
+                <div>
+                    <h3>{fetchTask.title}</h3>
+                    <p><strong>Description</strong>: <br /><br />{fetchTask.description}</p>
+                </div>
+            )
+        }
+    }
 
     // render task details
     return (
         <div>
-            <h3>Task: {fetchTask.title}</h3>
-            <p><strong>Description</strong>: {fetchTask.description}</p>
+            <h3>Task: {handleIds(id)}</h3>
         </div>
     );
 };
